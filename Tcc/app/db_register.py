675ca40e43cf5ea5c6b1ea1889a7420e67fd4803
Vinicius Silva <vinicius.silva@kumulus.com>
2022-11-16 18:20:34 -0300

    Modificadas algumas funcoes do banco

--- Tcc/app/db_register.py
+++ Tcc/app/db_register.py
@@ -1,30 +1,49 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, BigInteger, String, Date
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from AUTH import CONN_STR
 
 
-def cadastrar(nome, cpf, data_nasc, email, senha):
+def cadastrar(nome,email,cpf,data_nasc,senha,cargo_profissional,salario_mensal):
 
     cpf = int(cpf)
+    salario_mensal = float(salario_mensal)
 
     engine = create_engine(CONN_STR, echo=False)
     Sessions = sessionmaker(bind=engine)
     session = Sessions()
     Base = declarative_base()
 
-    class User(Base):
-        __tablename__ = 'FoodFinderUser'
+
+    class Usuario(Base):
+        __tablename__ = 'usuario'
+        children = relationship("Cartao")
 
         Nome = Column(String(255))
+        Email = Column(String(255))
         CPF = Column(BigInteger, primary_key=True)
         Data_Nasc = Column(String(255))
-        Email = Column(String(255))
         Senha = Column(String(255))
+        Cargo_Profissional = Column(String(255))
+        Salario_Mensal = Column(float)
+
+        
+
+    class Cartao(Base):
+        __tablename__ = 'cartao'
+
+        Nome_Titular = Column(String(255))
+        CPF_Titular = Column(BigInteger, ForeignKey("usuario.CPF"))
+        Numero_Cartao = Column(BigInteger)
+        Data_Vencimento = Column(String(255))
+        Tipo = Column(String(255))
+
+
 
     Base.metadata.create_all(engine)
-    ffUser = User(Nome=nome, CPF=cpf, Data_Nasc=data_nasc, Email=email, Senha=senha)
-    session.add(ffUser)
+    mfUser = Usuario(Nome=nome, CPF=cpf, Data_Nasc=data_nasc, Email=email, Senha=senha, Cargo_Profissional=cargo_profissional, Salario_Mensal=salario_mensal)
+    session.add(mfUser)
+    mfCartao = Cartao(Nome_Titular=nome, CPF_Titular)
     session.commit()
     session.close()
     success = "Cadastro concluido com sucesso!"
