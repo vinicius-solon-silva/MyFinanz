d7712dd018f8b1498584acda640dce5d5be0a297
Vinicius Silva <vinicius.silva@kumulus.com>
2022-11-29 16:55:49 -0300

    Adicionadas novas funcoes de cadastro

--- Tcc/app/db_register.py
+++ Tcc/app/db_register.py
@@ -36,30 +36,3 @@ def cadastrar_usuario(nome,email,cpf,data_nasc,senha,cargo_profissional,salario_
     success = "Cadastro concluido com sucesso!"
 
     return success
-
-
-def cadastrar_cartao(nome, cpf, numCartao, data, tipo):
-
-    cpf = int(cpf)
-
-    engine = create_engine(CONN_STR, echo=False)
-    Sessions = sessionmaker(bind=engine)
-    session = Sessions()
-    Base = declarative_base()
-
-    class Cartao(Base):
-        __tablename__ = 'cartao'
-
-        Nome_Titular = Column(String(255))
-        CPF_Titular = Column(BigInteger, ForeignKey("usuario.CPF"))
-        Numero_Cartao = Column(BigInteger)
-        Data_Vencimento = Column(String(255))
-        Tipo = Column(String(255))
-
-
-    Base.metadata.create_all(engine)
-    mfCartao = Cartao(Nome_Titular=nome, CPF_Titular=cpf, Numero_Cartao=numCartao, Data_Vencimento=data, Tipo=tipo)
-    session.add(mfCartao)
-    session.commit()
-    session.close()
-    success = "Cadastro concluido com sucesso!"
